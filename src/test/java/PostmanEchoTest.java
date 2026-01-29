14a029f750e1974e00435081e0126f5d55db5400
Mariya Buslaeva <buslaeva.mariya11@gmail.com>
2026-01-30 01:33:45 +0400

    test: fail for CI demonstration

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldReturnRussianData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("привет")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("пока"))
+        ;
+    }
+}
